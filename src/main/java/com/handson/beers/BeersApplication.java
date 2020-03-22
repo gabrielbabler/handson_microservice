8dbe83a432770c779145d302d97819f5d99452ab
Gabriel Babler <gabrielbabler@yahoo.com.br>
2020-03-22 16:26:15 -0300

    base project

---
+++ src/main/java/com/handson/beers/BeersApplication.java
@@ -0,0 +1,13 @@
+package com.handson.beers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeersApplication.class, args);
+	}
+
+}
