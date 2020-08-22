f7098cc842ee9c2172a723f0101f9f8feeef1f91
Gabriel Jacob Babler <gabriel.babler@sensedia.local>
2020-08-22 00:18:23 -0300

    added log on application

--- src/main/java/com/handson/beers/BeersApplication.java
+++ src/main/java/com/handson/beers/BeersApplication.java
@@ -1,15 +1,25 @@
 package com.handson.beers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.stream.Stream;
+
 @Slf4j
 @SpringBootApplication
-public class BeersApplication {
+public class BeersApplication implements ApplicationRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(BeersApplication.class, args);
 	}
-	log.info("The microservice handson-beers is started.");
 
+	@Override
+	public void run(ApplicationArguments applicationArguments) throws Exception {
+		log.info("--------------------- Application Arguments ---------------------");
+		Stream.of(applicationArguments.getSourceArgs()).forEach(log::info);
+		log.info("--------------------- Application Arguments ---------------------");
+		log.info("MS Beers successfully started!");
+	}
 }
