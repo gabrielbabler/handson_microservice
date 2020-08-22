15e861ca991a9b0a3de1ff7267751bca5b874a31
Gabriel Jacob Babler <gabriel.babler@sensedia.local>
2020-08-21 23:36:38 -0300

    added log on application

--- src/main/java/com/handson/beers/BeersApplication.java
+++ src/main/java/com/handson/beers/BeersApplication.java
@@ -1,13 +1,15 @@
 package com.handson.beers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class BeersApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BeersApplication.class, args);
 	}
+	log.info("The microservice handson-beers is started.");
 
 }
